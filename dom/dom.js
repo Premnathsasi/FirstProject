ff4efc747a192d64afe9f2be0a5536a953b2e4c3
Premnathsasi <premnathsasi4@gmail.com>
2023-01-04 11:41:10 +0530

    new file added

---
+++ dom/dom.js
@@ -0,0 +1,15 @@
+// console.log(document);
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.body);
+
+var titleHeader = document.querySelector('#header-title');
+titleHeader.style.borderBottom = "solid 3px black";
+
+var addItem = document.querySelector('.title');
+addItem.style.color = 'green ';
+addItem.style.fontWeight = 'bold';
+
+
+
+
