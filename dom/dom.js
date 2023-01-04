1327abcc99ae78a9db01cab6c6e584e849b14ae9
Premnathsasi <premnathsasi4@gmail.com>
2023-01-04 12:24:59 +0530

    modified using getElementsbyClassName

--- dom/dom.js
+++ dom/dom.js
@@ -1,15 +1,11 @@
-// console.log(document);
-// console.log(document.title);
-// console.log(document.head);
-// console.log(document.body);
 
-var titleHeader = document.querySelector('#header-title');
-titleHeader.style.borderBottom = "solid 3px black";
 
-var addItem = document.querySelector('.title');
-addItem.style.color = 'green ';
-addItem.style.fontWeight = 'bold';
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = 'green';
 
+for (let i=0; i<items.length;i++) {
+    items[i].style.color= '#ff0033';
+}
 
 
 
