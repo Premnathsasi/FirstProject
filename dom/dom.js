75c4f1cdb77048bc8749139cc7cc35cf25ca3f15
Premnathsasi <premnathsasi4@gmail.com>
2023-01-04 18:13:53 +0530

    creating Node and modifting

--- dom/dom.js
+++ dom/dom.js
@@ -23,20 +23,61 @@
 //         li[i].style.backgroundColor= '#f4f4f4';
 // }
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor= 'green';
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor= 'green';
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.visibility= 'hidden';
-
-
-var secondItem2 = document.querySelectorAll('.list-group-item')[1];
-secondItem2.style.color= 'green';
-
-var odd = document.querySelectorAll('li:nth-child(odd)');
-
-for (let i=0; i<odd.length;i++) {
-    odd[i].style.backgroundColor= 'green';
-}
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility= 'hidden';
 
 
+// var secondItem2 = document.querySelectorAll('.list-group-item')[1];
+// secondItem2.style.color= 'green';
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+
+// for (let i=0; i<odd.length;i++) {
+//     odd[i].style.backgroundColor= 'green';
+// }
+
+// parentElement 
+var itemList = document.querySelector('#items');
+ itemList.parentElement.style.backgroundColor= '#f4f4f4';
+
+//lastElementChild
+itemList.lastElementChild.style.backgroundColor='green';
+
+//lastChild
+console.log(itemList.lastChild);
+
+
+//firstChild
+console.log(itemList.firstChild);
+
+//firstElementChild
+itemList.firstElementChild.textContent= 'Hello';
+
+//nextSibling
+console.log(itemList.nextSibling);
+
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+
+//previous Sibling
+console.log(itemList.previousSibling);
+
+//previousElementSibling
+itemList.previousElementSibling.style.backgroundColor='yellow';
+
+//createElement
+var newList = document.createElement('li');
+newList.className="list5";
+console.log(newList);
+
+// Add Attribute
+newList.setAttribute('id','New List');
+
+//create Text Node
+var listText = document.createTextNode('item5');
+//append child
+newList.appendChild(listText);
+itemList.appendChild(newList);
