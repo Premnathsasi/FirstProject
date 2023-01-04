f81139eeac25cc7023b17b10f653ec4b1193340a
Premnathsasi <premnathsasi4@gmail.com>
2023-01-04 13:02:28 +0530

    modified getElementsByTagName

--- dom/dom.js
+++ dom/dom.js
@@ -1,11 +1,24 @@
+// console.log(document);
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.body);
+
+// var titleHeader = document.querySelector('#header-title');
+// titleHeader.style.borderBottom = "solid 3px black";
+
+// var addItem = document.querySelector('.title');
+// addItem.style.color = 'green ';
+// addItem.style.fontWeight = 'bold';
+
+// var items = document.getElementsByClassName("list-group-item");
+// items[2].style.backgroundColor = 'green';
+
+// for (let i=0; i<items.length;i++) {
+//     items[i].style.color= '#ff0033';
+// }
 
 
-var items = document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor = 'green';
-
-for (let i=0; i<items.length;i++) {
-    items[i].style.color= '#ff0033';
+var li = document.getElementsByTagName('li');
+for (let i=0; i<li.length;i++) {
+        li[i].style.backgroundColor= '#f4f4f4';
 }
-
-
-
