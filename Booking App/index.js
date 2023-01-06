a11e60a70f2c9e95e438a99eb587344124049a6c
Premnathsasi <premnathsasi4@gmail.com>
2023-01-06 15:56:35 +0530

    stores the user in right format

--- Booking App/index.js
+++ Booking App/index.js
@@ -14,10 +14,22 @@ function onSubmit(event) {
         setTimeout(() => msg.remove(),3000);
     } else {
         var li = document.createElement('li');
+        li.className= 'list-group-item';
         li.appendChild(document.createTextNode(`${nameInput.value} : ${mailInput.value} : ${phoneInput.value}`));
         userList.appendChild(li);
 
-        localStorage.setItem(mailInput.value,(`${'name'}:${nameInput.value}, ${'mail'}:${mailInput.value}, ${'Phone'}:${phoneInput.value}`));
+        let object1 = {
+            name: nameInput.value,
+            Email: mailInput.value,
+            Phone: phoneInput.value
+        }
+
+        let obj_serialised = JSON.stringify(object1);
+        localStorage.setItem(mailInput.value,obj_serialised);
+        let obj_deserialised = JSON.parse(localStorage.getItem(mailInput.value));
+        localStorage.getItem(obj_deserialised);
+
+        // localStorage.setItem(mailInput.value,(`${'name'}:${nameInput.value}, ${'mail'}:${mailInput.value}, ${'Phone'}:${phoneInput.value}`));
    
         nameInput.value='';
         mailInput.value='';
