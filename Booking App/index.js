43179e84f9cfbeca66c680bb509e52538edf3fa3
Premnathsasi <premnathsasi4@gmail.com>
2023-01-31 14:38:23 +0530

    saving user details on crud crud

--- Booking App/index.js
+++ Booking App/index.js
@@ -14,48 +14,61 @@ function onSubmit(event) {
         msg.innerHTML= "Please enter all fields";
         setTimeout(() => msg.remove(),3000);
     } else {
-        var li = document.createElement('li');
-        //Add del button
-        var delbtn = document.createElement('button');
-        delbtn.className= 'btn btn-danger float-end btn-sm delete';
-        delbtn.appendChild(document.createTextNode('Delete'));
-
-        //Add Edit button
-        var editBtn = document.createElement('button')
-        editBtn.className= 'btn btn-primary float-end btn-sm me-2 edit';
-        editBtn.appendChild(document.createTextNode('Edit'));
-
-
-        li.className= 'list-group-item';
-        li.appendChild(document.createTextNode(`${nameInput.value} - ${mailInput.value} - ${phoneInput.value}`));
-        li.appendChild(delbtn);
-        li.appendChild(editBtn);
-        userList.appendChild(li);
-
+        
         let object1 = {
             name: nameInput.value,
             Email: mailInput.value,
             Phone: phoneInput.value
         }
-        localStorage.setItem(object1.Email,JSON.stringify(object1));
 
-        delbtn.onclick= () => {
-            localStorage.removeItem(object1.Email);
-            userList.removeChild(li)
-        }
-        nameInput.value='';
-        mailInput.value='';
-        phoneInput.value='';
+        // localStorage.setItem(object1.Email,JSON.stringify(object1));
+        axios.post("https://crudcrud.com/api/f584a1e35bdd44058167e23488b4d4f5/appointmentData",object1)
+        .then(res => showOnscreen(res.data))
+        .catch((err) => {
+        document.body.innerHTML= document.body.innerHTML+'<h4>Error:something went wrong</h4>'
+            console.log(err)})
+        // showOnscreen(object1);
 
-        editBtn.onclick= () => {
-
-            nameInput.value= object1.name;
-            mailInput.value = object1.Email;
-            phoneInput.value=object1.Phone;
-            localStorage.removeItem(object1.Email);
-            userList.removeChild(li);
-        }
+        
 
     }
 
+}
+
+function showOnscreen(object1) {
+    
+    var li = document.createElement('li');
+    //Add del button
+    var delbtn = document.createElement('button');
+    delbtn.className= 'btn btn-danger float-end btn-sm delete';
+    delbtn.appendChild(document.createTextNode('Delete'));
+
+    //Add Edit button
+    var editBtn = document.createElement('button')
+    editBtn.className= 'btn btn-primary float-end btn-sm me-2 edit';
+    editBtn.appendChild(document.createTextNode('Edit'));
+
+
+    li.className= 'list-group-item';
+    li.appendChild(document.createTextNode(`${object1.name} - ${object1.Email} - ${object1.Phone}`));
+    li.appendChild(delbtn);
+    li.appendChild(editBtn);
+    userList.appendChild(li);
+
+    delbtn.onclick= () => {
+        localStorage.removeItem(object1.Email);
+        userList.removeChild(li)
+    }
+    nameInput.value='';
+    mailInput.value='';
+    phoneInput.value='';
+
+    editBtn.onclick= () => {
+
+        nameInput.value= object1.name;
+        mailInput.value = object1.Email;
+        phoneInput.value=object1.Phone;
+        localStorage.removeItem(object1.Email);
+        userList.removeChild(li);
+    }
 }
