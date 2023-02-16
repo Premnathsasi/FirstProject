c7192cb43f4cdaf8799e5a3df0bd939166a8cedf
Premnathsasi <premnathsasi4@gmail.com>
2023-02-16 21:00:47 +0530

    ExpenseDetails Added

--- React/src/components/ExpenseItem.js
+++ React/src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
-    return (
-        <div className='expense-item'>
-            <div>{props.date.toDateString()}</div>
-            <div className='location'> {props.location}</div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-
-        </div>
-    )
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        location={props.location}
+        title={props.title}
+        amount={props.amount}
+      />
+    </div>
+  );
 }
 
 export default ExpenseItem;
