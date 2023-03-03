15e830e3e806aead1fdb8fb6fac02a49223680fd
Premnathsasi <premnathsasi4@gmail.com>
2023-03-03 14:42:35 +0530

    meals summary added

--- Restaurant/src/App.js
+++ Restaurant/src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "./component/Header";
+import MealsSummary from "./component/meals/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header />
+    <MealsSummary />
     </Fragment>
   );
 }
