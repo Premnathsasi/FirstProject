92764103c12cc18b0922d4373d645b113231a6e2
Premnathsasi <premnathsasi4@gmail.com>
2023-03-03 13:31:29 +0530

    header

---
+++ Restaurant/src/component/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import HeaderButton from './HeaderButton';
+import mealsImage from './meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+    return <React.Fragment>
+        <header className={classes.header}>
+            <h1>Meatatarian</h1>
+            <HeaderButton/>
+        </header>
+        <div className={classes['main-image']}>
+    <img src={mealsImage} alt='Meals Images' />
+        </div>
+    </React.Fragment>
+};
+
+
+export default Header;
---
+++ Restaurant/src/component/HeaderButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCartShopping} from '@fortawesome/free-solid-svg-icons'
+import classes from './HeaderButton.module.css';
+
+const HeaderButton = (props) => {
+    return <button className={classes.button}>
+        <span  className={classes.icon}>
+        <FontAwesomeIcon icon={faCartShopping} />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>0</span>
+    </button>
+
+};
+
+export default HeaderButton;
